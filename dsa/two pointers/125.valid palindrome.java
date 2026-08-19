86eff457ec6e8ffc378d85229180b5786a1503db
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-19 18:54:24 +0530

    125.valid palindrome

---
+++ dsa/two pointers/125.valid palindrome.java
@@ -0,0 +1,26 @@
+"""
+
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+""";
+
+
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.strip();
+        s=s.toLowerCase();
+        int left=0,right=s.length()-1;
+        while(left<right){
+            while(left<right && !Character.isLetterOrDigit(s.charAt(left))) left++;
+            while(left<right && !Character.isLetterOrDigit(s.charAt(right))) right--;
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }
+            else return false;
+        }
+        return true;
+    }
+}
