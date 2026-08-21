da844c0ad5ac56af098f9353ac6a91f3ea8b7872
jayanithyan <srisarankamalyarns83@gmail.com>
2026-08-21 09:42:48 +0530

    658. Find K Closest Elements

---
+++ dsa/sliding window/658. Find K Closest Elements.java
@@ -0,0 +1,32 @@
+"""
+
+Given a sorted integer array arr, two integers k and x, return the k closest integers to x in the array. The result should also be sorted in ascending order.
+
+An integer a is closer to x than an integer b if:
+
+|a - x| < |b - x|, or
+|a - x| == |b - x| and a < b
+
+""";
+
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        int left=0;
+        int right=arr.length-k;
+        while(left<right){
+            int mid=left+(right-left)/2;
+            if(x-arr[mid]>arr[mid+k]-x){
+                left=mid+1;
+            }
+            else{
+                right=mid;
+            }
+        }
+        List<Integer> result=new ArrayList<>();
+        for(int i=left;i<left+k;i++){
+            result.add(arr[i]);
+        }
+        return result;
+    }
+}
