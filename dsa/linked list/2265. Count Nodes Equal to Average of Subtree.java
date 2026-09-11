b8bf446b0798f77a51f80601c74e7a1eea4a8deb
jayanithyan <srisarankamalyarns83@gmail.com>
2026-09-11 21:26:05 +0530

    2265. Count Nodes Equal to Average of Subtree

---
+++ dsa/linked list/2265. Count Nodes Equal to Average of Subtree.java
@@ -0,0 +1,43 @@
+"""
+
+Given the root of a binary tree, return the number of nodes where the value of the node is equal to the average of the values in its subtree.
+
+Note:
+
+The average of n elements is the sum of the n elements divided by n and rounded down to the nearest integer.
+A subtree of root is a tree consisting of root and all of its descendants.
+
+""";
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int ans = 0;
+    public int averageOfSubtree(TreeNode root) {
+        dfs(root);
+        return ans;
+    }
+    private int[] dfs(TreeNode node) {
+        if (node == null) return new int[]{0, 0};
+        int[] left = dfs(node.left);
+        int[] right = dfs(node.right);
+        int sum = left[0] + right[0] + node.val;
+        int cnt = left[1] + right[1] + 1;
+        if (sum / cnt == node.val) ans++;
+        return new int[]{sum, cnt};
+    }
+}
